698707aa42e17ff85d6198b8dabb53621b9607d4
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-10-25 11:51:01 +0100

    initial commit

---
+++ client_side_app/es-modules/workshop/calculator.js
@@ -0,0 +1,20 @@
+// import { add, subtract, multiply, divide } from './maths.js';
+import { add, subtract, multiply, divide } from './maths.js';
+
+function calculate(a, b, sign) {
+  switch (sign) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      throw new Error(`Sign '${sign}' not supported`);
+  }
+}
+
+// export { calculate };
+export default calculate;
